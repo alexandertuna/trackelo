1c4f3c448be3a17768a92802de7015562a4032c2
alexandertuna <alexander.tuna@gmail.com>
2023-11-21 11:20:14 +0100

    Simplify query a b it

--- query.py
+++ query.py
@@ -3,35 +3,41 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 def query(site):
-    with urlopen(site) as response:
-        soup = BeautifulSoup(response, 'html.parser')
+    try:
+        with urlopen(site) as response:
+            soup = BeautifulSoup(response, 'html.parser')
+    except:
+        print(f"Bad site, skipping: {site}")
+        return []
     return parse(soup)
 
 def parse(soup):
+    results = []
     for section in soup.find_all("section"):
-        racename = section.h2.string
+        eventname = section.h2.string
         for race in section:
-            table_headers = []
-            entries = []
+            racename = ""
+            rows = []
             for child in race:
                 if type(child) != bs4.element.Tag:
                     continue
-                if child.name == "span" and any(["EventResults_eventMeta" in obj for obj in child["class"]]):
-                    print(f"Race: {racename} {child.get_text()}")
-                if child.name == "div" and any(["EventResults_tableWrap" in obj for obj in child["class"]]):
-                    print(f"Result:")
+                if child.name == "span" and classContains(child, "EventResults_eventMeta"):
+                    racename = f"{eventname} {child.get_text()}"
+                    # print(f"Race: {racename}")
+                if child.name == "div" and classContains(child, "EventResults_tableWrap"):
+                    # print(f"Result:")
                     for row in child.find_all(attrs={"role": "row"}):
+                        rows.append([])
                         is_header = False
                         for ic, cell in enumerate(row):
-                            if cell.name == "th":
-                                table_headers.append(cell.get_text())
-                                is_header = True
-                            elif cell.name == "td":
-                                if ic == 0:
-                                    entries.append([])
-                                entries[-1].append(cell.get_text())
-                            else:
+                            if cell.name not in ["th", "td"]:
                                 fatal(f"Thats a weird cell: {cell.name}")
-                        print(table_headers if is_header else entries[-1])
-                        
-                            
+                            rows[-1].append(cell.get_text())
+                        # print(rows[-1])
+            if not racename:
+                continue
+            results.append( (racename, rows) )
+    return results
+
+def classContains(child, string):
+    return any([string in obj for obj in child["class"]])
